c25a81793d72aee1c289145aef3b92e3d45a8202
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-12-20 21:04:51 +0200

    Update make.sh

--- lib/make.sh
+++ lib/make.sh
@@ -1,34 +1,39 @@
-#!/usr/bin/env bash
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-echo $TF_INC
 
-CUDA_PATH=/usr/local/cuda/
+CUDA_PATH=/usr/local/cuda-8.0/
+CXXFLAGS=''
+
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+	CXXFLAGS+='-undefined dynamic_lookup'
+fi
 
 cd roi_pooling_layer
 
-nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
+if [ -d "$CUDA_PATH" ]; then
+	nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
+		-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CXXFLAGS \
+		-arch=sm_30
 
-## if you install tf using already-built binary, or gcc version 4.x, uncomment the two lines below
-#g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o roi_pooling.so roi_pooling_op.cc \
-#	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+	# g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
+	# 	roi_pooling_op.cu.o -I $TF_INC  -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
+	# 	-lcudart -L $CUDA_PATH/lib64
 
-# for gcc5-built tf
-g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=1 -o roi_pooling.so roi_pooling_op.cc \
-	roi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
-cd ..
+	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
+		roi_pooling_op.cu.o -I $TF_INC  -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS  -D_GLIBCXX_USE_CXX11_ABI=0 \
+		-lcudart -L $CUDA_PATH/lib64
 
-
-# add building psroi_pooling layer
-cd psroi_pooling_layer
-nvcc -std=c++11 -c -o psroi_pooling_op.cu.o psroi_pooling_op_gpu.cu.cc \
-	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
-
-g++ -std=c++11 -shared -o psroi_pooling.so psroi_pooling_op.cc \
-	psroi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
-
-## if you install tf using already-built binary, or gcc version 4.x, uncomment the two lines below
-#g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o psroi_pooling.so psroi_pooling_op.cc \
-#	psroi_pooling_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+else
+	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
+		-I $TF_INC -fPIC $CXXFLAGS
+fi
 
 cd ..
+
+#cd feature_extrapolating_layer
+
+#nvcc -std=c++11 -c -o feature_extrapolating_op.cu.o feature_extrapolating_op_gpu.cu.cc \
+#	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
+
+#g++ -std=c++11 -shared -o feature_extrapolating.so feature_extrapolating_op.cc \
+#	feature_extrapolating_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+#cd ..
