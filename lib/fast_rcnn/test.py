4ef3d06806aadbea826acfcb44f2188d482ea8d7
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2016-12-31 22:19:15 +0200

    Start implementing Faster-RCNN server

--- lib/fast_rcnn/test.py
+++ lib/fast_rcnn/test.py
@@ -335,3 +335,70 @@ def test_net(sess, net, imdb, weights_filename , max_per_image=300, thresh=0.05,
     print 'Evaluating detections'
     imdb.evaluate_detections(all_boxes, output_dir)
 
+
+# Returns the array of bounding boxes per classification per image.
+# It's a copy of test_net function but without visualizations.
+def get_bounding_boxes(sess, net, imdb, weights_filename , max_per_image=300, thresh=0.05):
+    """Test a Fast R-CNN network on an image database."""
+    num_images = len(imdb.image_index)
+    # all detections are collected into:
+    #    all_boxes[cls][image] = N x 5 array of detections in
+    #    (x1, y1, x2, y2, score)
+    all_boxes = [[[] for _ in xrange(num_images)]
+                 for _ in xrange(imdb.num_classes)]
+
+    output_dir = get_output_dir(imdb, weights_filename)
+    # timers
+    _t = {'im_detect' : Timer(), 'misc' : Timer()}
+
+    if not cfg.TEST.HAS_RPN:
+        roidb = imdb.roidb
+
+    det_file = os.path.join(output_dir, 'detections.pkl')
+
+    for i in xrange(num_images):
+        # filter out any ground truth boxes
+        if cfg.TEST.HAS_RPN:
+            box_proposals = None
+        else:
+            # The roidb may contain ground-truth rois (for example, if the roidb
+            # comes from the training or val split). We only want to evaluate
+            # detection on the *non*-ground-truth rois. We select those the rois
+            # that have the gt_classes field set to 0, which means there's no
+            # ground truth.
+            box_proposals = roidb[i]['boxes'][roidb[i]['gt_classes'] == 0]
+
+        im = cv2.imread(imdb.image_path_at(i))
+        _t['im_detect'].tic()
+        scores, boxes = im_detect(sess, net, im, box_proposals)
+        detect_time = _t['im_detect'].toc(average=False)
+
+        _t['misc'].tic()
+
+        # skip j = 0, because it's the background class
+        for j in xrange(1, imdb.num_classes):
+            inds = np.where(scores[:, j] > thresh)[0]
+            cls_scores = scores[inds, j]
+            cls_boxes = boxes[inds, j*4:(j+1)*4]
+            cls_dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])) \
+                .astype(np.float32, copy=False)
+            keep = nms(cls_dets, cfg.TEST.NMS)
+            cls_dets = cls_dets[keep, :]
+
+            all_boxes[j][i] = cls_dets
+
+        # Limit to max_per_image detections *over all classes*
+        if max_per_image > 0:
+            image_scores = np.hstack([all_boxes[j][i][:, -1]
+                                      for j in xrange(1, imdb.num_classes)])
+            if len(image_scores) > max_per_image:
+                image_thresh = np.sort(image_scores)[-max_per_image]
+                for j in xrange(1, imdb.num_classes):
+                    keep = np.where(all_boxes[j][i][:, -1] >= image_thresh)[0]
+                    all_boxes[j][i] = all_boxes[j][i][keep, :]
+        nms_time = _t['misc'].toc(average=False)
+
+        print 'im_detect: {:d}/{:d} {:.3f}s {:.3f}s' \
+              .format(i + 1, num_images, detect_time, nms_time)
+
+    return all_boxes
